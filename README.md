55459a02447702963ac8d7bd3fe8a4f2d74c32a4
funnyboy-roks <fbr@fbr.dev>
2026-08-07 19:18:31 -0500

    add codeberg

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # github-notif-indicator
 
 A bar module (specifically for [waybar], but should be generic) for
-showing the number of notifications on GitHub.
+showing the number of notifications on GitHub and Codeberg.
 
 This is basically the same as the [native github module] on polybar,
-just a lot less configurable
+just a lot less configurable (and it supports Codeberg)
 
 [waybar]: https://github.com/Alexays/Waybar
 [native github module]: https://github.com/polybar/polybar/wiki/Module:-github
@@ -23,6 +23,12 @@ To use in way bar, install and then add following custom module:
 
 ```jsonc
 "custom/github": {
-    "exec": "github-notif-indicator <path-to-secret> 10"
+    "exec": "github-notif-indicator <path-to-github-secret> <path-to-codeberg-secret> 10"
 }
 ```
+
+## Usage Note
+
+This application is very tailored to my needs.  If you intend to use it
+or want support for more software, please let me know and I may consider
+making this a little more polished.
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,82 @@
 use std::time::{Duration, Instant};
 
+use forgejo_api::Forgejo;
+use futures_util::StreamExt;
 use octocrab::Octocrab;
+
+pub async fn github_notifs(octocrab: &Octocrab) -> usize {
+    match octocrab
+        .activity()
+        .notifications()
+        .list()
+        .per_page(100)
+        .send()
+        .await
+    {
+        Ok(notifs) => notifs.into_stream(octocrab).count().await,
+        Err(e) => {
+            eprintln!("{:?}", e);
+            0
+        }
+    }
+}
+
+pub async fn codeberg_notifs(fj: &Forgejo) -> usize {
+    fj.notify_new_available()
+        .await
+        .unwrap()
+        .new
+        .unwrap_or_default() as _
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let mut args = std::env::args();
     let program = args.next().expect("arg0 is always set");
-    let (Some(token_path), Some(interval)) = (args.next(), args.next()) else {
-        eprintln!("Usage: {} <token-path> <interval (seconds)>", program);
+    let (Some(gh_token), Some(cb_token), Some(interval)) = (args.next(), args.next(), args.next())
+    else {
+        eprintln!(
+            "Usage: {} <gh-token-path> <cb-token-path> <interval (seconds)>",
+            program
+        );
         std::process::exit(1);
     };
     let interval = Duration::from_secs(interval.parse().unwrap());
 
-    let token = std::fs::read_to_string(token_path).unwrap();
+    let gh_token = std::fs::read_to_string(gh_token).unwrap();
+    let cb_token = std::fs::read_to_string(cb_token).unwrap();
 
     let octocrab = Octocrab::builder()
-        .personal_token(token.trim())
+        .personal_token(gh_token.trim())
         .set_connect_timeout(Some(Duration::from_secs(1)))
         .set_read_timeout(Some(Duration::from_secs(1)))
         .set_write_timeout(Some(Duration::from_secs(1)))
         .build()
         .unwrap();
 
+    let codeberg = Forgejo::new(
+        forgejo_api::Auth::Token(cb_token.trim()),
+        url::Url::parse("https://codeberg.org").unwrap(),
+    )
+    .unwrap();
+
     loop {
         let start = Instant::now();
-        match octocrab
-            .activity()
-            .notifications()
-            .list()
-            .per_page(100)
-            .send()
-            .await
-        {
-            Ok(notifs) => {
-                let notifs = notifs.into_iter().count();
-                if notifs == 0 {
-                    println!();
-                } else {
-                    println!(" {}", notifs);
-                }
-            }
-            Err(e) => {
-                eprintln!("{:?}", e);
-            }
+
+        let github = github_notifs(&octocrab).await;
+        if github > 0 {
+            print!(" {}", github);
         }
+
+        let codeberg = codeberg_notifs(&codeberg).await;
+        if codeberg > 0 {
+            if github > 0 {
+                print!("  ");
+            }
+            print!("  {}", codeberg);
+        }
+
+        println!();
 
         let elapsed = start.elapsed();
         tokio::time::sleep(interval.saturating_sub(elapsed)).await;
